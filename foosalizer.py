f80d2f9574339e8eafa1ae077be921739ffd4013
Edgard Lindner <elindner@gmail.com>
2009-01-14 22:49:39 +0000

    Cleaned up views, removed direct references to URLs

--- foosalizer.py
+++ foosalizer.py
@@ -39,7 +39,7 @@ class FoosalizerHandler(webapp.RequestHandler):
     self.response.out.write(template.render(path, data))
 
 
-class IndexHandler(FoosalizerHandler):
+class Index(FoosalizerHandler):
   def get(self):
     player = self.GetPlayer()
     show_all_matches = self.request.get('show_all_matches')
@@ -64,7 +64,7 @@ class IndexHandler(FoosalizerHandler):
     self.Render('index.html', data)
 
 
-class MatchHandler(FoosalizerHandler):
+class NewMatch(FoosalizerHandler):
   def get(self):
     query = Player.all()
     players = query.fetch(1000) # how many?
@@ -74,7 +74,7 @@ class MatchHandler(FoosalizerHandler):
     self.Render('match.html', data)
 
 
-class SaveMatchHandler(FoosalizerHandler):
+class SaveMatch(FoosalizerHandler):
   def post(self):
     player = self.GetPlayer()
     data = simplejson.loads(self.request.get('json_data'))
@@ -95,15 +95,14 @@ class SaveMatchHandler(FoosalizerHandler):
                   opponent_front=goal_data['opponent_front'])
       goal.put()
 
-    self.redirect('/results?match=%s' % match_key)
+    self.redirect(Results.get_url(match_key))
 
 
-class ResultsHandler(FoosalizerHandler):
-  def get(self):
-    match_key = db.Key(self.request.get('match'))
-    match = Match.get(match_key)
+class Results(FoosalizerHandler):
+  def get(self, match_key):
+    match = Match.get(db.Key(match_key))
     query = Goal.all()
-    query.filter('match =', match_key).order('time')
+    query.filter('match =', match.key()).order('time')
     goals = query.fetch(1000)  # how many?
     data = {
       'match': match,
@@ -112,22 +111,10 @@ class ResultsHandler(FoosalizerHandler):
     self.Render('results.html', data)
 
 
-class AnalysisHandler(FoosalizerHandler):
-  def get(self):
-    match_key = self.request.get('match')
-    player = self.request.get('player')
-
-    match = None
-    query = Goal.all()
-    if match_key != '':
-      match = Match.get(db.Key(match_key))
-      query.filter('match =', match)
-
-    if player not in ('', 'all'):
-      if player != 'all':
-        query.filter('player =', player)
-
-    goals = query.fetch(1000) # how many?
+class MatchAnalysis(FoosalizerHandler):
+  def get(self, match_key):
+    match = Match.get(db.Key(match_key))
+    goals = Goal.all().filter('match =', match).fetch(1000) # how many?
 
     stats = []
     for name, analysis in analyser.Analyse(goals).iteritems():
@@ -146,19 +133,48 @@ class AnalysisHandler(FoosalizerHandler):
       'match': match,
       'goals': goals,
       'stats': stats,
+    }
+
+    self.Render('match_analysis.html', data)
+
+
+class PlayerAnalysis(FoosalizerHandler):
+  def get(self, player):
+    query = Goal.all()
+    if player != 'all':
+      query.filter('player =', player)
+    query.fetch(1000)
+
+    stats = []
+    for name, analysis in analyser.Analyse(goals).iteritems():
+      pies = {'name': name, 'urls': []}
+      for player, data in analysis.iteritems():
+        pie = pygooglechart.PieChart3D(250, 100)
+        pie.set_pie_labels(str(data[1]))
+        pie.add_data(data[1])
+        pie.set_legend([str(x) for x in data[0]])
+        pie.set_title(player)
+        pies['urls'].append(pie.get_url())
+
+      stats.append(pies)
+
+    data = {
+      'goals': goals,
+      'stats': stats,
       'player': player
     }
 
-    self.Render('analysis.html', data)
+    self.Render('player_analysis.html', data)
 
 
 def main():
   handlers = [
-    ('/', IndexHandler),
-    ('/analysis', AnalysisHandler),
-    ('/results', ResultsHandler),
-    ('/match', MatchHandler),
-    ('/savematch', SaveMatchHandler),
+    ('/', Index),
+    ('/analysis/match/(\w+)', MatchAnalysis),
+    ('/analysis/player/(\w+)', PlayerAnalysis),
+    ('/results/(\w+)', Results),
+    ('/newmatch', NewMatch),
+    ('/savematch', SaveMatch),
   ]
   application = webapp.WSGIApplication(handlers, debug=True)
   wsgiref.handlers.CGIHandler().run(application)
